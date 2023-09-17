57f686b9f9963a382aea4d7e244fe213b82a4b07
Mustafizur Rahman <mustafizur232@gmail.com>
2023-09-18 01:33:12 +0600

    all practices done

--- first.js
+++ first.js
@@ -1,1 +1,52 @@
-console.log(550);
+
+var temp = 34;
+var weight = 80;
+var age = 26;
+var a = 56;
+var b = 36;
+var isLove = true;
+// console.log(typeof (isLove));
+// console.log(typeof (a))
+console.log(parseFloat(a + b));
+var c = 1.23;
+var d = 8.69;
+totalSum = c + d;
+// console.log(totalSum.toFixed(2));
+// console.log(parseInt(totalSum))
+var cat1 = 56;
+var cat2 = 'jontu';
+var catName = cat1 + cat2;
+// console.log(catName);
+
+
+
+var friendsName = ['mahfuz', 'murad', 'ahdan', 'hridoy', 'chuppu']
+
+friendsName[2] = 'ahsan';
+friendsName[2] = 'santu buri'
+friendsName[4] = 'tinku'
+// console.log(friendsName[1]);
+// console.log(friendsName.length)
+
+
+
+// a++;
+// console.log(a)
+
+
+
+// temp = temp + 6;
+// temp += 10;
+// age -= 6;
+// console.log(age)
+// console.log(temp)
+
+// console.log(age);
+// var total = age + weight;
+// console.log(total)
+// weight = 25;
+// console.log(weight);
+// var a = 56;
+// var b = 36;
+// var mod = a % b;
+// console.log(mod)
