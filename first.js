887d88ebec25f1e8f018ff23007ed262c609b127
Mustafizur Rahman <mustafizur232@gmail.com>
2023-09-19 13:43:33 +0600

    problem-1 solved

--- first.js
+++ first.js
@@ -1,34 +1,96 @@
+//------------- Problems-1----------------
+var fruits = ["apple", "banana", "Orange"];
+// index of Banana
+// var indexOfBanana = fruits.indexOf("banana");
+// console.log(indexOfBanana);
+// Replace banana with Mango
+// var bananaIndex = fruits.indexOf('banana');
+// fruits.splice(bananaIndex, 1, 'Mango');
+// console.log(fruits);
+// if (bananaIndex !== -1) {
+//     fruits[1] = 'Mango';
+// }
+// console.log(fruits);
 
-var temp = 34;
-var weight = 80;
-var age = 26;
-var a = 56;
-var b = 36;
-var isLove = true;
+// remove orange and add watermelon
+// fruits.pop('Orange');
+// console.log(fruits);
+// fruits.push('watermelon');
+// console.log(fruits);
+// delete fruits[2];
+// console.log(fruits);
+// fruits.shift()
+// fruits.unshift(34)
+console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 56;
+// var num2 = 98;
+// var num3 = 96;
+// var num4 = 91;
+// var num5 = 89;
+// var num6 = 12;
+// console.log(num1 > num2);
+// console.log(5 <= 6);
+// var temp = 34;
+// var weight = 80;
+// var age = 26;
+// var a = 56;
+// var b = 36;
+// var isLove = true;
 // console.log(typeof (isLove));
 // console.log(typeof (a))
 // console.log(parseFloat(a + b));
-var c = .5;
-var d = .2;
-totalSum = c + d;
+// var c = .5;
+// var d = .2;
+// totalSum = c + d;
 // console.log(totalSum);
 // console.log(totalSum.toFixed(2));
 // console.log(parseInt(totalSum))
-var cat1 = 56;
-var cat2 = 'jontu';
-var catName = cat1 + cat2;
+// var cat1 = 56;
+// var cat2 = 'jontu';
+// var catName = cat1 + cat2;
 // console.log(catName);
 
 
 
-var friendsName = ['mahfuz', 'murad', 'ahdan', 'hridoy', 'chuppu']
-var element = friendsName[3];
+// var friendsName = ['mahfuz', 'murad', 'ahdan', 'hridoy', 'chuppu']
+// var numbers = [25, 98, 36, 49, 21, 23, 51]
+// var add1 = numbers.push()
+// console.log(add1);
+// var del2 = numbers.pop()
+// console.log(del2);
+// console.log(numbers);
+// var updated = numbers.push(27);
+// console.log(numbers);
+// console.log(updated);
+
+
+
+
+// var element = friendsName[3];
 // console.log(element)
 
-friendsName[2] = 'ahsan';
-friendsName[2] = 'santu buri';
-friendsName[4] = 'tinku';
-var player = friendsName.indexOf('tinku');
+// friendsName[2] = 'ahsan';
+// friendsName[2] = 'santu buri';
+// friendsName[4] = 'tinku';
+// var player = friendsName.indexOf('tinku');
 // console.log(player)
 // console.log(friendsName[1]);
 // console.log(friendsName.length)
