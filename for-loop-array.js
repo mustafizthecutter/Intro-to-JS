b05cbf1e2e8f0bce013081a81d9e6c42e370ad03
Mustafizur Rahman <mustafizur232@gmail.com>
2023-09-21 03:29:23 +0600

    done for today

--- for-loop-array.js
+++ for-loop-array.js
@@ -15,14 +15,14 @@
 // for (i = 0; i < 7; i++) {
 //     console.log(i);
 // }
-var numbers = [25, 36, 98, 75, 68, 43, 16, 17, 13]
-for (i = 0; i < numbers.length; i++) {
-    var number = numbers[i];
-    if (number > 43) {
-        continue;
-    }
-    console.log(number);
-}
+// var numbers = [25, 36, 98, 75, 68, 43, 16, 17, 13]
+// for (i = 0; i < numbers.length; i++) {
+//     var number = numbers[i];
+//     if (number > 43) {
+//         continue;
+//     }
+//     console.log(number);
+// }
 
 // var cats = ['montu', 'jontu', 'pintu', 'tinku', 'chuppu', 'chomchom'];
 // for (i = 0; i < cats.length; i++) {
---
+++ phones.js
@@ -0,0 +1,7 @@
+var phones = ['samsung', 'nokia', 'xiaomi', 'iphone', 'oppo', 'vivo'];
+var i = 0;
+while (i < phones.length) {
+    var phone = phones[i];
+    console.log(phone);
+    i++;
+}
