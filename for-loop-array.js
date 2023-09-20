c631367c71994bdcf63f4ed9b2e10dee31e38e5c
Mustafizur Rahman <mustafizur232@gmail.com>
2023-09-20 20:56:45 +0600

    for loop with array done

---
+++ for-loop-array.js
@@ -0,0 +1,21 @@
+// var i = 0;
+// while (i < 7) {
+//     console.log(i);
+//     i++;
+// }
+// for (i = 0; i < 7; i++) {
+//     console.log(i);
+// }
+// for (i = 0; i < 7; i++) {
+//     console.log(i);
+// }
+// var numbers = [25, 36, 98, 75, 68, 43, 16, 17, 13]
+// for (i = 0; i < numbers.length; i++) {
+//     var number = numbers[i];
+//     console.log(number);
+// }
+var cats = ['montu', 'jontu', 'pintu', 'tinku', 'chuppu', 'chomchom'];
+for (i = 0; i < cats.length; i++) {
+    var cat = cats[i];
+    console.log(cat);
+}
