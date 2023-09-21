ec9b33c354cad20724093b3b7e754ead179b758a
Mustafizur Rahman <mustafizur232@gmail.com>
2023-09-21 10:43:24 +0600

    odd even done

--- even-odd-problems.js
+++ even-odd-problems.js
@@ -1,6 +1,18 @@
 // even numbers
-var number = 2;
-while (number <= 20) {
-    console.log(number);
-    number = number + 2
+// var number = 2;
+// while (number <= 20) {
+//     console.log(number);
+//     number = number + 2
+// }
+// find odd and even numbers from 1 to 7
+
+var a = 1;
+while (a <= 7) {
+    if (a % 2 == 0) {
+        console.log(a, 'is even');
+    }
+    else {
+        console.log(a, 'is odd');
+    }
+    a++;
 }
