72a8d9b22453030120a7dd46ad1b4413201b0503
Mustafizur Rahman <mustafizur232@gmail.com>
2023-09-24 01:09:17 +0600

    factorial done

---
+++ addition-multiplication-factorial.js
@@ -0,0 +1,83 @@
+// adding numbers by loop,basically for loop
+
+
+// let sum = 0;
+// for (let i = 1; i <= 7; i++) {
+//     sum = sum + i;
+//     console.log(sum, i);
+// }
+
+
+// adding numbers by function
+
+
+
+// function addition(numbers) {
+//     let sum = 0;
+//     for (let i = 1; i <= numbers; i++) {
+//         sum = sum + i;
+//         console.log(i, sum);
+//     }
+//     return sum;
+// }
+// const numAddition = addition(10);
+// console.log(numAddition);
+
+
+// multiplying Numbers by for loop
+
+
+// let multiply = 1;
+// for (let i = 1; i <= 5; i++) {
+//     multiply = multiply * i;
+//     console.log(i, multiply);
+// }
+
+
+
+// multiplying numbers by function
+
+
+
+// function multiplyNumbers(numbers) {
+//     let multiply = 1;
+//     for (let i = 1; i <= numbers; i++) {
+//         multiply = multiply * i;
+//         console.log(i, multiply);
+//     }
+//     return multiply;
+// }
+// const resultOfMultiply = multiplyNumbers(5);
+// console.log(resultOfMultiply);
+
+
+
+// factorial solving reverse way of 5!
+
+
+
+// function factorial(numbers) {
+//     let multiply = 1;
+//     for (let i = 1; i <= numbers; i++) {
+//         multiply = multiply * i;
+//         console.log(i, multiply);
+//     }
+//     return multiply;
+// }
+// const resultOfMultiply = factorial(5);
+// console.log(resultOfMultiply);
+
+
+
+// factorial solving normal way of 5!
+
+// function factorial(numbers) {
+//     let multiply = 1;
+//     for (let i = numbers; i >= 1; i--) {
+//         multiply = multiply * i;
+//         console.log(i, multiply);
+//     }
+//     return multiply;
+// }
+// const resultOfMultiply = factorial(5);
+// console.log(resultOfMultiply);
